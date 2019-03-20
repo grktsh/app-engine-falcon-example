5975820236004467972ad6ca5854ddeaf2308f8d
grktsh <grktsh@gmail.com>
2019-03-21 03:34:20 +0900

    Migrate from yaml to json for performance

--- packages/backend/app/api/wsgi.py
+++ packages/backend/app/api/wsgi.py
@@ -4,17 +4,17 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import json
 import os.path
 
-import yaml
 from falcon_oas import create_api
 
 from .v1 import version
 
 here = os.path.abspath(os.path.dirname(__file__))
-spec_path = os.path.join(here, '../../node_modules/api-docs/openapi.yaml')
+spec_path = os.path.join(here, '../../node_modules/api-docs/openapi.json')
 with open(spec_path) as f:
-    spec_dict = yaml.load(f)
+    spec_dict = json.load(f)
 
 app = create_api(spec_dict, base_module='app.api')
 app.add_route('/api/v1/version', version.Version())
